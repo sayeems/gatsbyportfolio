cfcc4cf37db51a2ce0b0e7a1e8ca19c9eed927d6
sayeem <sayeems@gmail.com>
2022-04-28 21:15:28 +0600

    test deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React from "react"
 import Layout from "../components/Layout"
+import big from "../assets/images/img_1.jpg"
+import { StaticImage } from "gatsby-plugin-image"
 
 export default function Home() {
   return (
     <Layout>
       <h1>Hi, I am Sayeem</h1>
+      {/* <img src={big} alt="myimg" style={{ maxWidth: "200px" }} /> */}
+      <StaticImage
+        src="../assets/images/img_1.jpg"
+        alt="myimg"
+        placeholder="tracedSVG"
+      />
     </Layout>
   )
 }
