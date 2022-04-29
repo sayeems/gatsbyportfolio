822afec57b8b5b03d441bb289a5b9b545b288e7f
sayeem <sayeems@gmail.com>
2022-04-30 00:24:43 +0600

    wp data source added

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,12 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+const url = process.env.WPGQL_URL || `https://${process.env.WPGQL_URL}/graphql`
+
 module.exports = {
   /* Your site config here */
   siteMetadata: {
@@ -15,5 +21,9 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: { url },
+    },
   ],
 }
